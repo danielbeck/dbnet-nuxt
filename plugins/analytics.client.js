68dfa9b38dd67d266c2e5bb45d884385375c7831
Daniel Beck <daniel@danielbeck.net>
2026-01-25 19:39:28 -0500

    analytics

--- plugins/analytics.client.js
+++ plugins/analytics.client.js
@@ -1,22 +1,26 @@
 import { defineNuxtPlugin } from '#app'
 import { initAnalytics, trackPageView } from '@/helpers/analytics.js'
 import { useUserStore } from '@/stores/user'
+import { useRouter } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
     if (process.server) return
 
-    try {
-        const userStore = useUserStore()
-        initAnalytics(userStore)
+    // Delay initialization until the app is mounted so Pinia and router are ready
+    nuxtApp.hook('app:mounted', () => {
         try {
-            const router = useRouter()
-            router.afterEach((to, from) => {
-                try { trackPageView(to, from) } catch (e) { }
-            })
+            const userStore = useUserStore()
+            initAnalytics(userStore)
+            try {
+                const router = useRouter()
+                router.afterEach((to, from) => {
+                    try { trackPageView(to, from) } catch (e) { }
+                })
+            } catch (e) {
+                // router not available, ignore
+            }
         } catch (e) {
-            // router not available, ignore
+            // ignore init errors
         }
-    } catch (e) {
-        // ignore init errors
-    }
+    })
 })
